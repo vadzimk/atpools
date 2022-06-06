c176f197a776e8b25fcc7be7046c96f8fa3b8d50
vadzim <vadzimk@hotmail.com>
2022-06-06 11:57:31 -0700

    complete mobile layout

--- main.js
+++ main.js
@@ -8,8 +8,8 @@ const preferDark = window.matchMedia('(prefers-color-scheme: dark)')
 //   theme.href = 'dark.css'
 // }
 
-btn.addEventListener('click', function (){
-
+btn.addEventListener('click', function (e){
+  e.stopPropagation()
   if(theme.getAttribute('href')==='light.css'){
 
     theme.href='dark.css'
@@ -17,4 +17,34 @@ btn.addEventListener('click', function (){
 
     theme.href='light.css'
   }
+})
+
+const inputs = document.querySelectorAll("input, textarea")
+
+inputs.forEach((input) => {
+  input.addEventListener("blur", (event) => {
+    if (event.target.value) {
+      input.classList.add("is-valid");
+    } else {
+      input.classList.remove("is-valid");
+    }
+  });
+});
+
+const images = document.querySelectorAll(".feature-img")
+
+images.forEach(image=>{
+  image.addEventListener('click', (e)=>{
+    e.stopPropagation()
+    image.classList.toggle("enlarged")
+  })
+})
+
+document.addEventListener('click', ()=>{
+
+  images.forEach(image=>{
+    if(image.classList.contains('enlarged')){
+      image.classList.remove('enlarged')
+    }
+  })
 })
