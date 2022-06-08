765fedc9b056ec474abc2d6312c0eaabefce1b65
vadzim <vadzimk@hotmail.com>
2022-06-07 23:22:17 -0700

    change email from

--- functions/_middleware.ts
+++ functions/_middleware.ts
@@ -6,13 +6,17 @@ export const onRequest = mailChannelsPlugin({
             to: [{name: "ACME Support", email: "atpoolsla@gmail.com"}],
         },
     ],
-    from: ({formData}) => {
-        const sender = formData.get('email')
-        return {
-            name: "Form submit",
-            email: `${sender}`,
-        }
+    from: {
+        name: "ACME Support",
+        email: "atpoolsla@gmail.com",
     },
+    // from: ({formData}) => {
+    //     //     const sender = formData.get('email')
+    //     //     return {
+    //     //         name: "Form submit",
+    //     //         email: `${sender}`,
+    //     //     }
+    //     // },
     respondWith: () => {
         return new Response(
             `Thank you for submitting your enquiry. A member of the team will be in touch shortly.`
