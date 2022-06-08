2de21d0c1d8c3f28b8888a362e458ed2e57b62a3
vadzim <vadzimk@hotmail.com>
2022-06-07 21:22:02 -0700

    replace response

--- functions/_middleware.ts
+++ functions/_middleware.ts
@@ -1,17 +1,18 @@
-import mailchannelsPlugin from "@cloudflare/pages-plugin-mailchannels";
+import mailChannelsPlugin from "@cloudflare/pages-plugin-mailchannels";
 
-export const onRequest = mailchannelsPlugin({
+export const onRequest = mailChannelsPlugin({
     personalizations: [
         {
             to: [{ name: "ACME Support", email: "atpoolsla@gmail.com" }],
         },
     ],
-    from: { name: "Enquiry", email: "no-reply@example.com" },
-    respondWith: ({ formData }) => {
-        // const replyMsg = `Hello, ${formData.get('email')}! Thank you, we will reply shortly`
-        return new Response(null, {
-            status: 302,
-            headers: {Location: "/thank-you"},
-        })
-    }
+    from: {
+        name: "ACME Support",
+        email: "support@example.com",
+    },
+    respondWith: () => {
+        return new Response(
+            `Thank you for submitting your enquiry. A member of the team will be in touch shortly.`
+        );
+    },
 });
