0a788c472b7e521780cd54dc9dc2083ed8ddb6cb
vadzim <vadzimk@hotmail.com>
2022-06-09 02:13:23 -0700

    add accessToken

--- netlify/functions/contact-form/index.js
+++ netlify/functions/contact-form/index.js
@@ -5,14 +5,18 @@ const nodemailer = require('nodemailer')
 dotenv.config()
 
 let transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
   auth: {
     type: 'OAuth2',
     user: process.env.MAIL_USERNAME,
     pass: process.env.MAIL_PASSWORD,
     clientId: process.env.OAUTH_CLIENTID,
     clientSecret: process.env.OAUTH_CLIENT_SECRET,
-    refreshToken: process.env.OAUTH_REFRESH_TOKEN
+    refreshToken: process.env.OAUTH_REFRESH_TOKEN,
+    accessToken: process.env.ACCESS_TOKEN,
+    expires: 1484314697598,
   }
 });
 
