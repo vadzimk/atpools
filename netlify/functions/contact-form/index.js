c335866ea9f71317528edad8cec7efdefaab11ac
vadzim <vadzimk@hotmail.com>
2022-06-09 00:03:41 -0700

    add netlify/functions/contact-form/index.js

--- netlify/functions/contact-form/index.js
+++ netlify/functions/contact-form/index.js
@@ -1,10 +1,48 @@
 // Docs on event and context https://www.netlify.com/docs/functions/#the-handler-method
+
+import dotenv from 'dotenv'
+import nodemailer from 'nodemailer';
+dotenv.config()
+
+let transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    type: 'OAuth2',
+    user: process.env.MAIL_USERNAME,
+    pass: process.env.MAIL_PASSWORD,
+    clientId: process.env.OAUTH_CLIENTID,
+    clientSecret: process.env.OAUTH_CLIENT_SECRET,
+    refreshToken: process.env.OAUTH_REFRESH_TOKEN
+  }
+});
+
+
+
 const handler = async (event) => {
+  // where to send the email and with what data:
+  let mailOptions = {
+    from: "'Site contact form' <atpoolsla@gmail.com>",
+    to: "vadzimkk@gmail.com, vadzimkk@gmail.com",
+    subject: 'Nodemailer Project',
+    text: 'Hi from your nodemailer project',
+    html: "<b>Hi from your nodemailer project</b>", // html body
+  };
+
   try {
     const subject = event.queryStringParameters.name || 'World'
+
+    // send mail with defined transport object
+    await transporter.sendMail(mailOptions,(err, data)=>{
+      if (err){
+        console.log("Error " + err)
+      } else {
+        console.log("Email sent successfully")
+      }
+    });
+
     return {
       statusCode: 200,
-      body: JSON.stringify({ message: `Hello ${subject}` }),
+      body: JSON.stringify({ message: `Sent` }),
       // // more keys you can return:
       // headers: { "headerName": "headerValue", ... },
       // isBase64Encoded: true,
